9a1425bcca8595cf00b212d9b46013e835d51c02
Chinhcrvc <vietchinhpd11082@gmail.com>
2025-06-20 18:19:46 +0700

    add product

--- src/main/java/com/datn/Service/ProductService.java
+++ src/main/java/com/datn/Service/ProductService.java
@@ -1,54 +1,61 @@
-// package com.datn.Service;
-// import java.util.List;
+package com.datn.Service;
 
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.Pageable;
-// import org.springframework.web.multipart.MultipartFile;
-// import com.datn.model.Product;
+import java.util.List;
 
-// public interface ProductService {
-// List<Product> findAll();
-// Product findByID(long id);
-// Product create(Product entity, MultipartFile imageFile);
-// void update(Product entity, MultipartFile imageFile,String oldImage);
-// void deleteById(long id);
-// boolean existsById(long id);
-// List<Product> findBestSellingProductPerCategory();
-// List<Product>findByProductOrderByQuantityDesc();
-// List<Product> findLatestProductsPerCategory();
-// List<Product> findProductByNameProductCategory(String name);
-// List<Product> findByCategory_Id(int id);
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+import com.datn.model.Product;
 
-// List<Product> findByProductCategoryId(int id);
+public interface ProductService {
+    List<Product> findAll();
 
-// Page<Product> findByPriceRange(
-// Integer productCategoryId,
-// Double minPrice,
-// Double maxPrice,
-// Pageable pageable
-// );
+    Product findByID(long id);
 
-// Page<Product> findByProductCategoryIdPage(
-// Integer productCategoryId,
-// Pageable pageable);
+    Product create(Product entity, MultipartFile imageFile);
 
-// Page<Product> findByColor(
-// Integer productCategoryId,
-// String color,
-// Pageable pageable
-// );
+    void update(Product entity, MultipartFile imageFile, String oldImage);
 
-// Page<Product> findByCaId(
-// Integer productCategoryId,
-// int categoryId,
-// Pageable pageable
-// );
+    void deleteById(long id);
 
-// // Page<Product> findBestSellingProductsByCategory( Integer
-// productCatelogyId,Pageable pageable);
+    boolean existsById(long id);
 
-// Page<Product> findByAllProduct(Pageable pageable);
+    List<Product> findBestSellingProductPerCategory();
 
-// Page<Product> searchByName(String name, Pageable pageable);
+    List<Product> findByProductOrderByQuantityDesc();
 
-// }
+    List<Product> findLatestProductsPerCategory();
+
+    List<Product> findProductByNameProductCategory(String name);
+
+    List<Product> findByCategory_Id(int id);
+
+    List<Product> findByProductCategoryId(int id);
+
+    Page<Product> findByPriceRange(
+            Integer productCategoryId,
+            Double minPrice,
+            Double maxPrice,
+            Pageable pageable);
+
+    Page<Product> findByProductCategoryIdPage(
+            Integer productCategoryId,
+            Pageable pageable);
+
+    Page<Product> findByColor(
+            Integer productCategoryId,
+            String color,
+            Pageable pageable);
+
+    Page<Product> findByCaId(
+            Integer productCategoryId,
+            int categoryId,
+            Pageable pageable);
+
+    // Page<Product> findBestSellingProductsByCategory( Integer
+    // productCatelogyId,Pageable pageable);
+    Page<Product> findByAllProduct(Pageable pageable);
+
+    Page<Product> searchByName(String name, Pageable pageable);
+
+}
--- src/main/java/com/datn/dao/ProductDAO.java
+++ src/main/java/com/datn/dao/ProductDAO.java
@@ -1,83 +1,74 @@
-// package com.datn.dao;
+package com.datn.dao;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.Pageable;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-// import com.datn.model.Product;
+import com.datn.model.Product;
 
-// public interface ProductDAO extends JpaRepository<Product, Long> {
-// @Query(value = "SELECT * FROM products p WHERE p.id IN " +
-// "(SELECT MAX(p2.id) FROM products p2 GROUP BY p2.product_Category_Id)",
-// nativeQuery = true)
-// List<Product> findLatestProductsPerCategory();
+public interface ProductDAO extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM products p WHERE p.id IN " +
+            "(SELECT MAX(p2.id) FROM products p2 GROUP BY p2.product_Category_Id)", nativeQuery = true)
+    List<Product> findLatestProductsPerCategory();
 
-// @Query("SELECT p FROM Product p WHERE p.productCategory.name LIKE :name order
-// by p.id desc")
-// List<Product> findProductByNameProductCategory(@Param("name") String name);
+    @Query("SELECT p FROM Product p WHERE p.productCategory.name LIKE :name order by p.id desc")
+    List<Product> findProductByNameProductCategory(@Param("name") String name);
 
-// @Query(value = "SELECT p.* FROM products p " +
-// "JOIN (SELECT p2.product_Category_Id, MAX(p2.quantity) AS max_quantity " +
-// "FROM products p2 GROUP BY p2.product_Category_Id) temp " +
-// "ON p.product_Category_Id = temp.product_Category_Id " +
-// "AND p.quantity = temp.max_quantity", nativeQuery = true)
-// // so luong nhieu nhat
-// List<Product> findBestSellingProductPerCategory();
+    @Query(value = "SELECT p.* FROM products p " +
+            "JOIN (SELECT p2.product_Category_Id, MAX(p2.quantity) AS max_quantity " +
+            "FROM products p2 GROUP BY p2.product_Category_Id) temp " +
+            "ON p.product_Category_Id = temp.product_Category_Id " +
+            "AND p.quantity = temp.max_quantity", nativeQuery = true)
+    // so luong nhieu nhat
+    List<Product> findBestSellingProductPerCategory();
 
-// @Query("select p from Product p order by p.quantity desc")
-// List<Product> findByProductOrderByQuantityDesc();
+    @Query("select p from Product p order by p.quantity desc")
+    List<Product> findByProductOrderByQuantityDesc();
 
-// List<Product> findByCategory_Id(int categoryId);
+    List<Product> findByCategory_Id(int categoryId);
 
-// List<Product> findByProductCategoryId(int productCategoryId);
+    List<Product> findByProductCategoryId(int productCategoryId);
 
-// @Query("SELECT p FROM Product p WHERE p.productCategory.id =
-// :productCategoryId " +
-// "AND p.price BETWEEN :minPrice AND :maxPrice")
+    @Query("SELECT p FROM Product p WHERE p.productCategory.id = :productCategoryId " +
+            "AND p.price BETWEEN :minPrice AND :maxPrice")
 
-// Page<Product> findByPriceRange(
-// @Param("productCategoryId") Integer productCategoryId,
-// @Param("minPrice") Double minPrice,
-// @Param("maxPrice") Double maxPrice,
-// Pageable pageable);
+    Page<Product> findByPriceRange(
+            @Param("productCategoryId") Integer productCategoryId,
+            @Param("minPrice") Double minPrice,
+            @Param("maxPrice") Double maxPrice,
+            Pageable pageable);
 
-// Page<Product> findByProductCategoryId(
-// @Param("productCategoryId") Integer productCategoryId,
-// Pageable pageable);
+    Page<Product> findByProductCategoryId(
+            @Param("productCategoryId") Integer productCategoryId,
+            Pageable pageable);
 
-// @Query("SELECT p FROM Product p WHERE p.productCategory.id =
-// :productCategoryId " +
-// "AND p.color.name LIKE %:color%")
+    @Query("SELECT p FROM Product p WHERE p.productCategory.id = :productCategoryId " +
+            "AND p.color.name LIKE %:color%")
 
-// Page<Product> findByColor(
-// @Param("productCategoryId") Integer productCategoryId,
-// @Param("color") String color,
-// Pageable pageable);
+    Page<Product> findByColor(
+            @Param("productCategoryId") Integer productCategoryId,
+            @Param("color") String color,
+            Pageable pageable);
 
-// @Query("SELECT p FROM Product p WHERE p.productCategory.id =
-// :productCategoryId " +
-// "AND p.category.id=:categoryId")
+    @Query("SELECT p FROM Product p WHERE p.productCategory.id = :productCategoryId " +
+            "AND p.category.id=:categoryId")
 
-// Page<Product> findByCaId(
-// @Param("productCategoryId") Integer productCategoryId,
-// @Param("categoryId") Integer categoryId,
-// Pageable pageable);
+    Page<Product> findByCaId(
+            @Param("productCategoryId") Integer productCategoryId,
+            @Param("categoryId") Integer categoryId,
+            Pageable pageable);
 
-// // @Query("SELECT p FROM Product p JOIN OrderDetail od ON p.id =
-// od.product.id
-// // WHERE p.productCategory.id = :productCategoryId GROUP BY p.id ORDER BY
-// // SUM(od.quantity) DESC")
-// // Page<Product>
-// findBestSellingProductsByCategory(@Param("productCategoryId")
-// // Integer productCategoryId,Pageable pageable);
+    // @Query("SELECT p FROM Product p JOIN OrderDetail od ON p.id = od.product.id
+    // WHERE p.productCategory.id = :productCategoryId GROUP BY p.id ORDER BY
+    // SUM(od.quantity) DESC")
+    // Page<Product> findBestSellingProductsByCategory(@Param("productCategoryId")
+    // Integer productCategoryId,Pageable pageable);
 
-// @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%',
-// :keyword, '%'))")
-// Page<Product> searchByName(@Param("keyword") String keyword, Pageable
-// pageable);
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Product> searchByName(@Param("keyword") String keyword, Pageable pageable);
 
-// }
+}
