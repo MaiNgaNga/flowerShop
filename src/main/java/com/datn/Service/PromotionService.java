746baf3825b7be16559128b11a21b4eec67422a2
giahieu <hieungpd10871@gmail.com>
2025-07-03 20:06:21 +0700

    add promotions

---
+++ src/main/java/com/datn/Service/PromotionService.java
@@ -0,0 +1,25 @@
+package com.datn.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.datn.model.Promotion;
+
+public interface PromotionService {
+
+    List<Promotion> findAll();
+    Promotion findByID(String id);
+    Promotion create(Promotion entity);
+    void update(Promotion entity);
+    void deleteById(String id);
+    boolean existsById(String id);
+    List<Promotion> getPromotionsInCurrentMonth();
+    Page<Promotion> findByAllPromotion(Pageable pageable);
+    Page<Promotion> searchByDate(LocalDate fromDate, LocalDate toDate, Pageable pageable);
+    Page<Promotion> searchByName(String name, Pageable pageable);
+    
+} 
